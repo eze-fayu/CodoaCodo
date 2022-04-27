18cdc05c9b61687201992cb770ca40cbcc3e2a59
Ezequiel Faiella <ezequielfaiella@gmail.com>
2022-04-27 15:23:26 -0300

    empezamos con javascript

---
+++ Javascript/clase 14/script.js
@@ -0,0 +1,18 @@
+console.log("hola mundo")
+console.info("informacion")
+console.warn("advertencia")
+console.error("msj de error")
+document.write("Escribiendo desde Javascript")
+
+console.log("%c¡Hola Manz!",
+            "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
+
+// document.write("%c¡Hola Manz!",
+//             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");     /* esto no funciona*/
+
+// alert("alerta!!!!!!!!!!")
+
+
+document.writeln("esto baja una linea")
+document.write("<h1>JAVAAAAAAAAA</h1>")
+
