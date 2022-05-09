89db38c7f9243b0a842f842080ec33ec00dcbc0e
Ezequiel Faiella <ezequielfaiella@gmail.com>
2022-05-09 11:53:08 -0300

    falta telefono

--- web.js
+++ web.js
@@ -1,49 +1,55 @@
+const formulario = document.getElementById("form");
+
+
 function valida_envia(){
 
     const userName = document.getElementById("nombre");
     const userEmail = document.getElementById("email");
-    const usertel = document.getElementById("telefono");
-    const userasunto = document.getElementById("asunto");
-    const usermsj = document.getElementById("mensaje");
-    console.log(userName)
-    
+    const userTel = document.getElementById("telefono");
+    const userAsunto = document.getElementById("asunto");
+    const userMsj = document.getElementById("mensaje");
 
-    /*
-    if (userName.value.length==0){
-           alert("Tiene que escribir su nombre")
-           document.formulario.nombre.focus()
-           return 0;
-    }
+    // console.log(userName)
+    console.log(userTel)
+    // console.log(userEmail)
+    // console.log(userAsunto)
+    // console.log(userMsj)
 
-    if (usertel.lengh>10){
+    // if (userName.value.length==0){
+    //        alert("Tiene que escribir su nombre")
+    //        userName.focus()
+    //     //    return 0;
+    // }
+
+    if ( typeof userTel.value === 'undefined' || userTel.value === null || userTel.value.lengh < 10 ){
         alert("Tiene que introducir un número telefonico válido.")
-        usertel.focus()
-        return 0;
+        userTel.focus()
+        // return 0;
     }
 
-    if (!userEmail.match( /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ )){
-        alert("Tiene que introducir un email válido.")
-        userEmail.focus()
-        return 0;        
-    }
+    // if (!userEmail.value.match( /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ )){
+    //     alert("Tiene que introducir un email válido.")
+    //     userEmail.focus()
+    //     // return 0;        
+    // }
     
-    if (userasunto.value.length==0){
-        alert("Tiene que escribir el asunto")
-        userasunto.focus()
-        return 0;
-    }
+    // if (userAsunto.value.length==0){
+    //     alert("Tiene que escribir el asunto")
+    //     userAsunto.focus()
+    //     // return 0;
+    // }
 
-    if (usermsj.value.length==0){
-        alert("Tiene que escribir el mensaje")
-        usermsj.focus()
-        return 0;
-    }
+    // if (userMsj.value.length==0){
+    //     alert("Tiene que escribir el mensaje")
+    //     userMsj.focus()
+    //     // return 0;
+    // }
   
     //el formulario se envia
-    alert("Muchas gracias por enviar el formulario");
+    // alert("Muchas gracias por enviar el formulario");
     // document.formulario.submit();
-*/
+
 }
 
 
-document.getElementById("form").addEventListener('submit', valida_envia());
+// formulario.addEventListener('submit', valida_envia());
