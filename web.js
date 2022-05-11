f3b77a228c9aeaa7d01299339af0ce9edfe956ca
Fayu <r826720@hotmail.com>
2022-05-10 22:04:12 -0300

    ok validacion tel. falta volver al formulario

--- web.js
+++ web.js
@@ -10,46 +10,47 @@ function valida_envia(){
     const userMsj = document.getElementById("mensaje");
 
     // console.log(userName)
-    console.log(userTel)
+    // console.log("Valor de usertel: ",userTel.value)
+    // console.log("tamaño de usertel", userTel.value.lenght)
     // console.log(userEmail)
     // console.log(userAsunto)
     // console.log(userMsj)
 
-    // if (userName.value.length==0){
-    //        alert("Tiene que escribir su nombre")
-    //        userName.focus()
-    //     //    return 0;
-    // }
-
-    if ( typeof userTel.value === 'undefined' || userTel.value === null || userTel.value.lengh < 10 ){
-        alert("Tiene que introducir un número telefonico válido.")
-        userTel.focus()
-        // return 0;
+    if (userName.value.length==0){
+        alert("Tiene que escribir su nombre")
+        userName.focus()
+        return 0;
     }
 
-    // if (!userEmail.value.match( /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ )){
-    //     alert("Tiene que introducir un email válido.")
-    //     userEmail.focus()
-    //     // return 0;        
-    // }
-    
-    // if (userAsunto.value.length==0){
-    //     alert("Tiene que escribir el asunto")
-    //     userAsunto.focus()
-    //     // return 0;
-    // }
+    if ( typeof userTel.value === 'undefined' || userTel.value === null || userTel.value.length < 10 ){
+        alert("Tiene que introducir un número telefonico válido.")
+        userTel.focus()
+        return 0;
+    }
 
-    // if (userMsj.value.length==0){
-    //     alert("Tiene que escribir el mensaje")
-    //     userMsj.focus()
-    //     // return 0;
-    // }
+    if (!userEmail.value.match( /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ )){
+        alert("Tiene que introducir un email válido.")
+        userEmail.focus()
+        return 0;        
+    }
+    
+    if (userAsunto.value.length==0){
+        alert("Tiene que escribir el asunto")
+        userAsunto.focus()
+        return 0;
+    }
+
+    if (userMsj.value.length==0){
+        alert("Tiene que escribir el mensaje")
+        userMsj.focus()
+        return 0;
+    }
   
-    //el formulario se envia
-    // alert("Muchas gracias por enviar el formulario");
-    // document.formulario.submit();
+    // el formulario se envia
+    alert("Muchas gracias por enviar el formulario");
+    document.formulario.submit();
 
 }
 
 
-// formulario.addEventListener('submit', valida_envia());
+formulario.addEventListener('submit', valida_envia());
